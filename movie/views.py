8347be7dc0c0517019c96373682ec6d750983783
zainabmeerza <zainabmeerza@gmail.com>
2022-06-15 14:03:27 +0000

    create working pagination for search results

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-
+from movie.models import Movie
 import requests
 
 # Create your views here.
@@ -16,6 +16,7 @@ def index(request):
         context = {
             'query': query,
             'movie_data': movie_data,
+            'page_number':1,
         }
 
         template = loader.get_template('search_results.html')
@@ -24,3 +25,19 @@ def index(request):
     
     return render(request, 'index.html')
 
+
+def pagination(request, query, page_number):
+    url = 'https://www.omdbapi.com/?apikey=9c393141&s=' + query + '&page=' + str(page_number)
+    response = requests.get(url)
+    movie_data = response.json()
+    page_number = int(page_number) + 1
+
+    context = {
+        'query': query,
+        'movie_data': movie_data,
+        'page_number': page_number,
+    }
+
+    template = loader.get_template('search_results.html')
+
+    return HttpResponse(template.render(context, request))
