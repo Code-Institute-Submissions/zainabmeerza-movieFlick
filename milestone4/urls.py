2e64015b502469d219824f77524b4d38792f50a5
zainabmeerza <zainabmeerza@gmail.com>
2022-07-04 17:22:14 +0000

    fix profile template

--- milestone4/urls.py
+++ milestone4/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from userauth.views import UserProfile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movie/', include('movie.urls')),
     path('actors/', include('actor.urls')),
     path('account/', include('userauth.urls')),
+    path('<username>/', UserProfile, name='profile')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
