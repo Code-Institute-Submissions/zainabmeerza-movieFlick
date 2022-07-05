e297ae239cfea6ac3f045bea480bbb87061d81b3
zainabmeerza <zainabmeerza@gmail.com>
2022-07-05 11:03:16 +0000

    changed to settings

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
+development = os.environ.get('DEVELOPMENT', False)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,16 +23,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fmu-i&h9m7z*e$!6@ur9%f+tja5oxfscbpc)teg+v+p-i4_u7(')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-fmu-i&h9m7z*e$!6@ur9%f+tja5oxfscbpc)teg+v+p-i4_u7(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = development
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'localhost', '127.0.0.1']
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-zainabmeerza-movieflick-0egr5vzp7d5.ws-eu51.gitpod.io','http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-zainabmeerza-movieflick-0egr5vzp7d5.ws-eu51.gitpod.io', 'http://127.0.0.1:8000/']
 # Application definition
 
 INSTALLED_APPS = [
@@ -62,7 +68,7 @@ ROOT_URLCONF = 'milestone4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'milestone4/templates' )],
+        'DIRS': [os.path.join(BASE_DIR, 'milestone4/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,16 +87,17 @@ WSGI_APPLICATION = 'milestone4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
@@ -128,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR,'milestone4/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'milestone4/static')
 ]
 
 
